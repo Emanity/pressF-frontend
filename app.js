49493a2ca42849fadebffb1856504c3271177656
John Walker <john.walker@kainos.com>
2021-08-23 15:50:43 +0100

    Changes to enable nunjucks, CSS & Bootstrap

--- app.js
+++ app.js
@@ -2,18 +2,37 @@ const express = require('express')
 const app = express() 
 const nunjucks = require('nunjucks');
 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Port
+let port = 7999;
+
+//Configuring Express to use Nunjucks
 nunjucks.configure('views', { 
          express: app 
 }); 
- 
+
+// Provide Public files such as Images & Styling
+app.use(express.static('public'))
+
+// Nunjucks view engine
+//app.set('view engine', 'njk');
 app.set('view engine', 'html');
 
 
 app.get('/', function (req, res) { 
-   res.send('<h1>First message from Express</h1>') 
+   res.render('index')
+   console.log('Request processed'); 
+}); 
+
+app.get('/index', function (req, res) { 
+   res.render('index')
    console.log('Request processed'); 
 }); 
  
-app.listen(7999, function() { 
-   console.log('Express started') 
+// Start Server
+app.listen(port, function() {
+   console.log('Express started')
 });
