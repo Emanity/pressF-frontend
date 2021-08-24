5b6e028f88fa10bb845aa6f3a0bfc57366a56ade
walkinator16000 <43759504+walkinator16000@users.noreply.github.com>
2021-08-24 14:51:54 +0100

    Remove commented URL

--- JobRoles.js
+++ JobRoles.js
@@ -1,11 +1,10 @@
 const fetch = require('node-fetch');
 
-
 exports.getJobRoles = async () => {
 
-api_url = 'http://localhost:8080/api/getjobroles' //'http://localhost:8080/api/getjobroles'
+    api_url = 'http://localhost:8080/api/getjobroles' 
 
-const fetch_response = await fetch(api_url);
-return  await fetch_response.json();
+    const fetch_response = await fetch(api_url);
+    return  await fetch_response.json();
 
 }
