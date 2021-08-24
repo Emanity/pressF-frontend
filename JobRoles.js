bcb6e857c3172bccf50995409a8b809100726b2c
walkinator16000 <43759504+walkinator16000@users.noreply.github.com>
2021-08-24 14:12:09 +0100

    Styling Changes & + EndPoint for Job-Roles US001

--- JobRoles.js
+++ JobRoles.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 
 exports.getJobRoles = async () => {
 
-api_url = 'http://localhost:8080/api/testMybatis'
+api_url = 'http://localhost:8080/api/getjobroles'
 
 const fetch_response = await fetch(api_url);
 return  await fetch_response.json();
