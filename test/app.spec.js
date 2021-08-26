fa3cd4fbf82c219106538dcca533d27b425d0b50
walkinator16000 <43759504+walkinator16000@users.noreply.github.com>
2021-08-26 16:23:22 +0100

    Test to check JobRoles Page returns!!!

--- test/app.spec.js
+++ test/app.spec.js
@@ -2,15 +2,16 @@ var supertest = require('supertest');
 const assert = require('assert');
 const app = require('../app');
 const fetch = require('node-fetch')
-const JobRoles = require('../JobRoles')
 
-jest.mock('node-fetch', ()=> jest.fn())
 
 describe('index', function() {
+    
     var request
     
     beforeEach(function () {
+
         request = supertest(app)
+
     })
 
     describe('GET /', function() {
@@ -22,76 +23,35 @@ describe('index', function() {
                 assert.equal(response.statusCode, 200)
                 
             })
+
         })
+
     })
 
-    // describe('GET /job-roles', function() {
+})
+
+    describe('job-roles', function() {
+
+        var request
         
-    //     it('should get job roles', async() => {
+        beforeEach(function () {
 
-    //         axios.get.mockResolvedValue({
-    //             data: [
-    //                 {
-    //                     jobid:1,
-    //                     jobTitle: '(CTO) Chief Technology Officer'
-    //                 },
-    //                 {
-    //                     jobid:2,
-    //                     jobTitle: 'Innovation Lead'
-    //                 }
-    //             ]
-    //         })
+            request = supertest(app)
 
-    //         const title = await getFirstJobTitle()
-    //         expect(title).toEqual('(CTO) Chief Technology Officer')
-    //     })
-    // })
+        })
 
+    describe('GET /job-roles', function() {
+        
+        it('should return OK status', function(){
+        return request
+            .get('/')
+            .then(function(response){
+                assert.equal(response.statusCode, 200)
+                
+            })
 
+        })
 
-
-})
-
-
-// const unmockedFetch = global.fetch
-
-// beforeAll(() => {
-//   global.fetch = () =>
-//     Promise.resolve({
-//       json: () => Promise.resolve([]),
-//     })
-// })
-
-// afterAll(() => {
-//   global.fetch = unmockedFetch
-// })
-
-// // This is actual testing suite
-// describe('getJobRoles', () => {
-//   it('works', async () => {
-//     const json = await JobRoles.getJobRoles()
-//     expect(Array.isArray(json)).toEqual(true)
-//     expect(json.length).toEqual(0)
-//   })
-// })
-
-
-const unmockedFetch = global.fetch
-
-beforeAll(() => {
-    global.fetch = () => 
-    Promise.resolve({
-        json: () => Promise.resolve([]),
     })
-})
 
-afterAll(() => {
-    global.fetch = unmockedFetch
-})
-
-describe('withFetch', () => {
-    test('works', async () => {
-        const json = await JobRoles.getJobRoles()
-        expect (Array.isArray(json)).toEqual(true)
-    })
 })
