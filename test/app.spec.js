5489abf8c89fc1cbf44f661cff12bf148679ff9a
walkinator16000 <43759504+walkinator16000@users.noreply.github.com>
2021-08-27 11:59:35 +0100

    2 more tests for Job Role Details

--- test/app.spec.js
+++ test/app.spec.js
@@ -62,7 +62,7 @@ describe('index', function() {
 
 })
 
-//Job Roles Details Test
+//Job Roles Details Test Job Role Exists
 describe('job-role-details', function() {
 
     var request
@@ -73,13 +73,75 @@ describe('job-role-details', function() {
 
     })
 
-describe('GET /job-role-details', function() {
+describe('GET /job-role-details Role Exists', function() {
     
     it('should return OK status', async () => {
 
     return await request
 
         .get('/job-role-details/1')
+
+        .then(function(response){
+
+            assert.equal(response.statusCode, 200)
+            
+        })
+        
+    })
+
+})
+
+})
+
+//Job Role Details Do Not Exist
+describe('job-role-details', function() {
+
+    var request
+    
+    beforeEach(function () {
+
+        request = supertest(app)
+
+    })
+
+describe('GET /job-role-details Role Does Not Exist', function() {
+    
+    it('should return OK status', async () => {
+
+    return await request
+
+        .get('/job-role-details/2000')
+
+        .then(function(response){
+
+            assert.equal(response.statusCode, 200)
+            
+        })
+        
+    })
+
+})
+
+})
+
+//Job Role Details Do Not Exist (Text passed in instead of number)
+describe('job-role-details', function() {
+
+    var request
+    
+    beforeEach(function () {
+
+        request = supertest(app)
+
+    })
+
+describe('GET /job-role-details Role Does Not Exist (text)', function() {
+    
+    it('should return OK status', async () => {
+
+    return await request
+
+        .get('/job-role-details/hi')
 
         .then(function(response){
 
