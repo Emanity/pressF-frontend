82edd70df2873a1c41d1383843a4e3d0b77fa7bf
Aleksandra Łobocka <aleksandra.lobocka@kainos.com>
2021-08-29 22:26:59 +0200

    UI home page tests extended.

--- cypress_e2e/Tests/home_page.js
+++ cypress_e2e/Tests/home_page.js
@@ -2,7 +2,17 @@ const { expect } = require("chai");
 
 describe("Loading home page", function(){
     it("connects to index.html", function(){
-        cy.visit("http://localhost:7999/index");
+        cy.visit("http://localhost:7999");
+        cy.url().should('include', '/');
     });
 });
+
+describe("Getting Job Roles view", function(){
+    it("connects to display Job Roles", function(){
+        cy.visit("http://localhost:7999");
+        cy.get('.btn').contains('View Job Roles').click();
+        cy.url().should('include', '/job-roles');
+    });
+});
+
 
