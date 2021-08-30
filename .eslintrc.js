251ff3e8cabd4a8d8dc9ff2c8469051bf0a82601
Eman Abbasi <eman.abbasi@kainos.com>
2021-08-30 16:36:51 +0100

    added more eslint config for auto fix formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,45 @@ module.exports = {
 		],
 		'jest/expect-expect': [
 			'off'
-		]
+		],
+		'padded-blocks': [
+			'error', {
+				'classes': 'always',
+				'blocks': 'never',
+				'switches': 'always'
+			}
+		],
+		'no-multiple-empty-lines': [
+			'error', {
+				'max': 1,
+				'maxEOF': 0
+			}
+		],
+		'lines-around-comment': [
+			'error', {
+				'beforeBlockComment': true,
+				'beforeLineComment': false
+			}
+		],
+		'padding-line-between-statements': [
+			'error', 
+			{blankLine: 'always', prev: 'cjs-import', next: 'expression'},
+			{blankLine: 'never', prev: 'cjs-import', next: 'cjs-import'},
+			{blankLine: 'always',    prev: '*', next: 'block-like'},
+			{blankLine: 'always',    prev: '*', next: 'multiline-block-like'}
+		],
+		'spaced-comment': [
+			'error',
+			'always', {
+			'line': {
+				'markers': ['/'],
+				'exceptions': ['-', '+']
+			},
+			'block': {
+				'markers': ['!'],
+				'exceptions': ['*'],
+				'balanced': true
+			}
+		}]
 	}
 };
