713c54d30ec466f9e4a5c5a5984054fc17ed972b
Eman Abbasi <eman.abbasi@kainos.com>
2021-08-30 14:54:14 +0100

    added linting - can be run using npm run lint

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:jest/recommended'
+	],
+	'plugins': [
+		'jest'
+	],
+	'parserOptions': {
+		'ecmaVersion': 12
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'jest/expect-expect': [
+			'off'
+		]
+	}
+};
--- app/app.js
+++ app/app.js
@@ -1,8 +1,6 @@
-const express = require('express') 
-const app = express() 
+const express = require('express'); 
+const app = express(); 
 const nunjucks = require('nunjucks');
-const fetch = require('node-fetch');
-var path = require('path')
 
 const bodyParser = require('body-parser');
 const JobRoles = require('./JobRoles');
@@ -11,45 +9,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Configuring Express to use Nunjucks
 nunjucks.configure('views', {
-         autoescape: true,
-         express: app 
+	autoescape: true,
+	express: app 
 }); 
 
 // Provide Public files such as Images & Styling
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Nunjucks view engine
 app.set('view engine', 'html');
 
 // Index (Home Page) Route
 app.get('/', function (req, res) { 
-   res.render('index')
-   console.log('Request processed'); 
+	res.render('index');
+	console.log('Request processed'); 
 }); 
 
 // Index (Home Page) Route
 app.get('/index', function (req, res) {
-   res.render('index')
-   console.log('Request processed'); 
+	res.render('index');
+	console.log('Request processed'); 
 }); 
 
 // Job Roles Route
 app.get('/job-roles', async (req, res) => {
-
-   let result = await JobRoles.getJobRoles()
-   res.render('job-roles', {JobRoles : result});
-   
-})
+	let result = await JobRoles.getJobRoles();
+	res.render('job-roles', {JobRoles : result});
+});
 
 // Index (Home Page) Route
 app.get('/job-role-details/:jobRoleID', async (req, res) => {
-   
-   jobRoleID = req.params.jobRoleID;
-
-   let result = await JobRoles.getJobRoleDetails(jobRoleID);
-
-   res.render('job-role-details', {JobRoles : result})
-
-})
+	var jobRoleID = req.params.jobRoleID;
+	let result = await JobRoles.getJobRoleDetails(jobRoleID);
+	res.render('job-role-details', {JobRoles : result});
+});
 
 module.exports = app;
