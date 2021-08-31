462828e69fea7f86e4f200fc58096ff725932314
Aleksandra Łobocka <aleksandra.lobocka@kainos.com>
2021-08-31 17:40:39 +0200

    job_roles.js tests updated.

--- cypress_e2e/Tests/job_roles.js
+++ cypress_e2e/Tests/job_roles.js
@@ -8,11 +8,21 @@ describe("Getting Job Roles view", function(){
     });
 });
 
-describe("Viewing available job role's details", function(){
-    it("displays available job role's details", function(){
-        cy.visit("http://localhost:7999/job-roles");
-        cy.url().should('include', '/job-roles');
-        cy.get('#accordion2').contains("Innovation Lead").click();
-        cy.get('a[href*="/job-roles/2"]').contains('View Job Role Details').click()
+describe("Viewing available job role's summary", function(){
+    it("displays available job role's summary", function(){
+        cy.get('#list2').contains('Innovation Lead').click();
+        cy.get('a[href*="/job-role-details/2"]').contains('View Job Role Details').click();
     });
 });
+
+describe("Viewing job role's details", function(){
+    it("displays job role's details", function(){
+        cy.get('#card-text').contains('Innovation Lead');
+        cy.get('#jobRoleDetails').contains('Job Role Details').click();
+        cy.get('#jobCompetencies').should('have.text', 'Job Competencies').click();
+        cy.get('#jobCompLink').should('be.visible');
+        cy.get('#jobSpecification').should('have.text', 'Job Specification').click();
+        cy.get('#jobSpecLink').should('be.visible');
+    });
+});
+
