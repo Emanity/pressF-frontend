30ff2459ec1ce7a55abbbab31e3b20abe985e706
Aleksandra Łobocka <aleksandra.lobocka@kainos.com>
2021-09-02 10:12:19 +0200

    job-roles.js updated. Views updated added 'data-cy' attribute.

--- cypress_e2e/Tests/job_roles.js
+++ cypress_e2e/Tests/job_roles.js
@@ -1,28 +1,32 @@
 const { expect } = require("chai");
 
-describe("Getting Job Roles view", function(){
-    it("connects to display Job Roles", function(){
+describe("Getting Job Roles view", () => {
+    it("connects to display Job Roles", () => {
         cy.visit("http://localhost:7999");
-        cy.get('#jobRoles-Button').should('contain', 'View Job Roles').click();
+        cy.get('[data-cy=viewJobRoles]').should('contain', 'View Job Roles').click();
         cy.url().should('include', '/job-roles');
     });
 });
 
-describe("Viewing available job role's summary", function(){
-    it("displays available job role's summary", function(){
-        cy.get('#list2').should('contain', 'Innovation Lead').click();
-        cy.get('a[href*="/job-role-details/2"]').should('contain', 'View Job Role Details').click();
+describe("Viewing available job role's summary", () => {
+    it("displays available job role's summary", () => {
+        cy.visit('http://localhost:7999/job-roles');
+        cy.get('[data-cy="jobSummary2"]').should('contain', 'Innovation Lead').click();
+        cy.get('[data-cy=jobRoleDetailsLink2]').should('contain', 'View Job Role Details').click();
     });
 });
 
-describe("Viewing job role's details", function(){
-    it("displays job role's details", function(){
-        cy.get('#card-text').should('contain', 'Innovation Lead');
-        cy.get('#jobRoleDetails').should('contain', 'Job Role Details').click();
-        cy.get('#jobCompetencies').should('contain', 'Job Competencies').click();
-        cy.get('#jobCompLink').should('be.visible');
-        cy.get('#jobSpecification').should('contain', 'Job Specification').click();
-        cy.get('#jobSpecLink').should('be.visible');
+describe("Viewing job role's details", () => {
+    it("displays job role's details", () => {
+        cy.visit('http://localhost:7999/job-role-details/2');
+        cy.get('[data-cy=jobTitleCard]').should('contain', 'Innovation Lead');
+        cy.get('[data-cy=jobRoleDetails]').should('contain', 'Job Role Details').click();
+        cy.get('[data-cy=jobCompetencies]').should('contain', 'Job Competencies').click();
+        cy.get('[data-cy=jobCompLink]').should('be.visible');
+        cy.get('[data-cy=jobSpecification]').should('contain', 'Job Specification').click();
+        cy.get('[data-cy=jobSpecLink]').should('be.visible');
     });
 });
+
+
 
