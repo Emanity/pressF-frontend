8723b25166ce02e5f12053e77df4b8eae23c1b12
Felix Moore <felixmoore115@gmail.com>
2021-09-02 12:06:37 +0100

    Added try/catch to handle API connection failures

--- app/JobRoles.js
+++ app/JobRoles.js
@@ -3,37 +3,43 @@ const fetch = require('node-fetch');
 
 // Method that calls API and returns Job Roles list
 exports.getJobRoles = async () => {
-	var api_url = 'http://localhost:8080/api/getjobroles';
-
-	const fetch_response = await fetch(api_url);
-
-	// If statement to catch errors
-	if (fetch_response.status >= 200 && fetch_response.status <= 299) {
+	try {
+		var api_url = 'http://localhost:8080/api/getjobroles';
+		const fetch_response = await fetch(api_url);
+	
+		// If statement to catch errors
+		if (fetch_response.status >= 200 && fetch_response.status <= 299) {
 		// Return response
-		console.log('Job Roles Fetched');
-		return await fetch_response.json();
-	} else {
+			console.log('Job Roles Fetched');
+			return await fetch_response.json();
+		} else {
 		// Return NULL and log error
-		console.log(fetch_response.status, ' | ' ,  fetch_response.statusText, ' | Unable to Fetch Job Roles');
-		return null;
+			console.log(fetch_response.status, ' | ' ,  fetch_response.statusText, ' | Unable to Fetch Job Roles');
+			return null;
+		}
+	} catch(err) {
+		return 'Error: There was a problem retrieving job roles.'; 
 	}
 };
 
 // Method that calls API and returns the Job Role Details for the Job Role ID
 exports.getJobRoleDetails = async ( jobRoleID ) => {
-	var api_url = `http://localhost:8080/api/getjobroledetails/${jobRoleID}`;
+	try {
+		var api_url = `http://localhost:8080/api/getjobroledetails/${jobRoleID}`;
+		const fetch_response = await fetch(api_url);
 
-	const fetch_response = await fetch(api_url);
-
-	// If statement to catch errors
-	if (fetch_response.status >= 200 && fetch_response.status <= 299) {
+		// If statement to catch errors
+		if (fetch_response.status >= 200 && fetch_response.status <= 299) {
 		// Return response
-		console.log('Job Spec Fetched');
-		return await fetch_response.json();
-	}
-	else {
+			console.log('Job Spec Fetched');
+			return await fetch_response.json();
+		}
+		else {
 		// Return NULL and log error
-		console.log(fetch_response.status, ' | ', fetch_response.statusText, ' | Unable to fetch Job Role Details ');
-		return null;
+			console.log(fetch_response.status, ' | ', fetch_response.statusText, ' | Unable to fetch Job Role Details ');
+			return null;
+		}
+	} catch(err) {
+		return 'Error: There was a problem retrieving job role details.'; 
 	}
 };
