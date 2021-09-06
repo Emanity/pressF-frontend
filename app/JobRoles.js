157fde168969e3008b22141081c60ae86a83223f
walkinator16000 <43759504+walkinator16000@users.noreply.github.com>
2021-09-06 14:27:25 +0100

    add-job-role frontend completed

--- app/JobRoles.js
+++ app/JobRoles.js
@@ -45,6 +45,72 @@ exports.getJobRoleDetails = async ( jobRoleID ) => {
 	}
 };
 
+/* Method that calls API and returns Job bands */
+exports.getJobBand = async () => {
+	try {
+		var api_url = 'http://localhost:8080/api/getjobband';
+		const fetch_response = await fetch(api_url);
+	
+		// If statement to catch errors
+		if (fetch_response.status >= 200 && fetch_response.status <= 299) {
+		// Return response
+			console.log('Job Bands Fetched');
+			return await fetch_response.json();
+		} else {
+		// Return NULL and log error
+			console.log(fetch_response.status, ' | ' ,  fetch_response.statusText, ' | Unable to Fetch Job Bands');
+			return null;
+		}
+	} catch(err) {
+		console.log('API connection failed: ' + err);
+		return 'Error: There was a problem retrieving Job Bands.'; 
+	}
+};
+
+/* Method that calls API and returns Job capabilities */
+exports.getJobCapability = async () => {
+	try {
+		var api_url = 'http://localhost:8080/api/getjobcapability';
+		const fetch_response = await fetch(api_url);
+	
+		// If statement to catch errors
+		if (fetch_response.status >= 200 && fetch_response.status <= 299) {
+		// Return response
+			console.log('Job Capabilities Fetched');
+			return await fetch_response.json();
+		} else {
+		// Return NULL and log error
+			console.log(fetch_response.status, ' | ' ,  fetch_response.statusText, ' | Unable to Fetch Job Capabilities');
+			return null;
+		}
+	} catch(err) {
+		console.log('API connection failed: ' + err);
+		return 'Error: There was a problem retrieving job Capabilities.'; 
+	}
+};
+
+/* Method that calls API and returns Job disciplines */
+exports.getJobDiscipline = async () => {
+	try {
+		var api_url = 'http://localhost:8080/api/getjobdiscipline';
+		const fetch_response = await fetch(api_url);
+	
+		// If statement to catch errors
+		if (fetch_response.status >= 200 && fetch_response.status <= 299) {
+		// Return response
+			console.log('Job Disciplines Fetched');
+			return await fetch_response.json();
+		} else {
+		// Return NULL and log error
+			console.log(fetch_response.status, ' | ' ,  fetch_response.statusText, ' | Unable to Fetch Job Disciplines');
+			return null;
+		}
+	} catch(err) {
+		console.log('API connection failed: ' + err);
+		return 'Error: There was a problem retrieving job disciplines.'; 
+	}
+};
+
 
 /* Method that adds a job role */
 exports.addJobRole = async (jobRole) => {
@@ -69,11 +135,41 @@ exports.addJobRole = async (jobRole) => {
 				console.log('Job Spec Fetched');
 		} else {
 			// Return NULL and log error
-			console.log(fetch_response.status, ' | ', fetch_response.statusText, ' | Unable to fetch Job Role Details ');
+			console.log(fetch_response.status, ' | ', fetch_response.statusText, ' | Unable to add a Job Role');
 		}
 
 	} catch (err) {
 		return 'Error: There was a problem adding the job role.'
 	}
+};
 
+/* Method that adds a job role */
+exports.addJobBand = async (jobBand) => {
+	try {
+		// API url for adding a job role
+		var api_url = 'http://localhost:8080/api/addjobrole'
+		// data from post request
+		var data = jobBand
+		// creating POST request to POST to API
+		const options = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		}
+		// POSTing to API
+		const fetch_response = await fetch(api_url, options);
+		// If statement to catch errors
+		if (fetch_response.status >= 200 && fetch_response.status <= 299) {
+			// Return response
+				console.log('Job Spec Fetched');
+		} else {
+			// Return NULL and log error
+			console.log(fetch_response.status, ' | ', fetch_response.statusText, ' | Unable to add a Job Role');
+		}
+
+	} catch (err) {
+		return 'Error: There was a problem adding the job role.'
+	}
 };
--- app/app.js
+++ app/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator');
 const JobRoles = require('./JobRoles');
-const FormValidation = require('./FormValidation');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -48,36 +48,76 @@ app.get('/login', function (req, res) {
 	res.render('login');
 });
 
+/* Add Job Band Routes */
+// GET route for add-job-band form
 app.get('/add-job-band', function (req, res) {
 	res.render('add-job-band');
 });
+
+/* POST route to validate and send add-job-band form */
+app.post('/add-job-band', [
+	check('jobBand', 'Job Band: Must contain letters').exists().isAlpha(),
+	check('jobBand', 'Job Band: Must be longer than 0').isLength({min: 1})
+], async (req, res) => {
+	//variable to store error details
+	errors = validationResult(req);
+	// check to ensure there are no errors
+	if (!errors.isEmpty()){
+		const alert = errors.array();
+		// if there are errors return details to the form and the details
+		res.render('add-job-band', {jobBand : req.body, alert});
+	} else {
+		// send the body to addJobBand function in JobRoles.js
+		await JobRoles.addJobBand(req.body);
+		res.redirect('index');
+		console.log('add-job-band: POST Sent')
+	}
+
+})
 
 app.get('/add-job-capability', function (req, res) {
 	res.render('add-job-capability');
 });
 
-
 /* Add Job Role Routes */
-app.get('/add-job-role', function (req, res) {
-	res.render('add-job-role');
+// GET route for add-job-role form
+app.get('/add-job-role', async (req, res) => {
+	let bandResult = await JobRoles.getJobBand();
+	let capabilityResult = await JobRoles.getJobCapability();
+	let disciplineResult = await JobRoles.getJobDiscipline();
+	res.render('add-job-role', {bands : bandResult, capabilities : capabilityResult, disciplines : disciplineResult});
 });
 
-app.post('/add-job-role', async (req, res) => {
+/* POST route to validate and send add-job-role form */
+app.post('/add-job-role', [
+	check('jobTitle', 'Job Title: Must be contain letters').exists().isAlpha(),
+	check('jobTitle', 'Job Title: Must be longer than 0').isLength({min: 1}),
+	check('jobBand', 'Job Band: Must contain letters').exists().isAlpha(),
+	check('jobBand', 'Job Band: Must be longer than 0').isLength({min: 1}),
+	check('jobCapability', 'Job Capability: Must contain letters').exists().isAlpha(),
+	check('jobCapability', 'Job Capability: Must be longer than 0').isLength({min: 1}),
+	check('jobSpecification', 'Job Specification: Must contain letters').exists().isAlpha(),
+	check('jobSpecification', 'Job Specification: Must be longer than 0').isLength({min: 1}),
+	check('jobDiscipline', 'Job Discipline: Must contain letters').exists().isAlpha(),
+	check('jobDiscipline', 'Job Discipline: Must be longer than 0').isLength({min: 1}),
+	check('jobSpecification', 'Job Specificaion: Must contain letters').exists().isAlpha(),
+	check('jobSpecification', 'Job Specificaion: Must be longer than 0').isLength({min: 1})
+], async (req, res) => {
 	// variable to store error details
-	error = "";
-
+	errors = validationResult(req);
 	// check to ensure there are no errors
-	if (error.length > 0)
-	{
+	if (!errors.isEmpty()){
+		const alert = errors.array();
 		// if there are errors return the details to the form and error
-		res.render('add-job-role', {JobRole : req.body, error: error});
+		res.render('add-job-role', {jobRole : req.body, alert});
 	} else {
 		// send the body to addJobRole function in JobRoles.js
+		console.log(req.body);
 		await JobRoles.addJobRole(req.body);
 		res.redirect('index');
+		console.log('add-job-role: POST Sent')
 	}
-
-})
+});
 
 
 module.exports = app;
