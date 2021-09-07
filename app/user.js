a6346c306c2097e8437fd4dcc754e6f0d5580d43
Eman Abbasi <eman.abbasi@kainos.com>
2021-09-07 12:04:45 +0100

    added user profile and logout button

--- app/user.js
+++ app/user.js
@@ -1,31 +1,36 @@
 // Node fetch
 const fetch = require('node-fetch');
 const crypto = require('crypto');
-
+const Buffer = require('buffer');
 
 this.exampleUser = {
-		email: null,
-		role: null
-	};
+	email: null,
+	role: null
+};
 
 exports.updateUser = (req, role) => {
-	this.exampleUser.email = req.body.email
-	this.exampleUser.role = role
-}
+	this.exampleUser.email = req.body.email;
+	this.exampleUser.role = role;
+};
 
-exports.getUser = () => { return this.exampleUser}
+exports.resetUser = () => {
+	this.exampleUser.email = null;
+	this.exampleUser.role = null;
+};
+
+exports.getUser = () => { return this.exampleUser;};
 
 // Gets response to attempted login from API
 exports.getLoginResponse = async (req) => {
 	try {
 		let hash = crypto.createHash('sha256').update(req.body.password).digest('hex');
 		let data = Buffer.from(req.body.email + ':' + hash).toString('base64');
-		const api_url = `http://localhost:8080/api/login`;
+		const api_url = 'http://localhost:8080/api/login';
 		const fetch_response = await fetch(api_url, {
-            method: 'POST',
-        	headers: {'Authorization': 'Basic ' + data},
+			method: 'POST',
+			headers: {'Authorization': 'Basic ' + data},
 			credentials: 'include'
-        });
+		});
 
 		// If statement to catch errors
 		if (fetch_response.status >= 200 && fetch_response.status <= 299) {
