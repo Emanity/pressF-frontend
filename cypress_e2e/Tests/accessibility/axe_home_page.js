d4e88244c632a4246d6c7b7d94008198de59f691
Aleksandra Łobocka <aleksandra.lobocka@kainos.com>
2021-09-09 09:45:57 +0200

    Accessibility and e2e tests added. Added data-cy to views. Fixed views to pass accessibility tests.

---
+++ cypress_e2e/Tests/accessibility/axe_home_page.js
@@ -0,0 +1,19 @@
+const { expect } = require("chai");
+
+describe("Loading home page", function(){
+    it("connects to index.html", function(){
+        cy.visit("http://localhost:7999");
+        cy.url().should('include', '/');
+    });
+
+    describe('A11y',() => {
+        beforeEach(() => {
+            cy.injectAxe();
+        });
+    
+        it('should have no detectable a11y errors on page load', () => {
+            cy.screenshot('home-page-axe');
+            cy.checkA11y();
+        });
+    });
+});
